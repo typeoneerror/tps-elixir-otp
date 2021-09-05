c2ef7552799804e0661b2dd0ed60df2e85b0ea58
Benjamin Borowski <ben@weareokidoki.com>
2021-09-04 21:54:52 -0700

    Completes chapter 6

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,6 +2,7 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
@@ -16,8 +17,18 @@ defmodule Servy.Handler do
     %{ method: method, path: path, resp_body: "" }
   end
 
+  def log(conv), do: IO.inspect(conv)
+
   def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
     %{ conv | resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
   end
 
   def format_response(conv) do
@@ -35,16 +46,32 @@ request = """
 GET /wildthings HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
-Accept: */*
+  Accept: */*
 
 """
 
-expected_response = """
-HTTP/1.1 200 OK
-Content-Type: text/html
-Content-Length: 20
+response = Servy.Handler.handle(request)
 
-Bears, Lions, Tigers
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+  Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+  Accept: */*
+
 """
 
 response = Servy.Handler.handle(request)
